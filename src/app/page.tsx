9e76ded5e814f68028f5c8006b8877b69d2981a5
s1ddh-rth <siddharth19n@gmail.com>
2025-06-02 11:34:20 +0100

    remove snap scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,16 @@ import TeamSection from "@/components/team-section";
 
 export default function Home() {
   return (
-    <main className="h-screen overflow-y-auto snap-y snap-mandatory">
+    <main className="min-h-screen">
       <Navbar className="fixed top-0 left-0 right-0 z-50" />
       <div className="pt-16 md:pt-20">
-        <div className="snap-start">
+        <div>
           <HeroSection />
         </div>
-        <div className="snap-start">
+        <div>
           <Features />
         </div>
-        <div className="snap-start">
+        <div>
           <TeamSection />
         </div>
       </div>
